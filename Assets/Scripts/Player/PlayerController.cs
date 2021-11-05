68220c652ce421605f3d64f9e95347819949a4d3
kajdemuynck <kaj.de.muynck@student.howest.be>
2021-11-05 15:35:03 +0100

    Added player controls template v001

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravity;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+
+    private Vector3 velocity;
+    private float groundDistance = 0.4f;
+    private bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z).normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
